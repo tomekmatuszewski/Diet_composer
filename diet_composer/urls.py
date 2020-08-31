734b7fb823e9dfb5885e889417dc4bd1cd763d12
TomekM <tomasz_matuszewski@o2.pl>
2020-08-31 23:15:15 +0200

    templates modification

--- diet_composer/urls.py
+++ diet_composer/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='diet-composer-home'),
-    path('about/', views.about, name='diet-composer-about'),
+    path('', views.home, name='diet_composer-home'),
+    path('about/', views.about, name='diet_composer-about'),
 ]
--- diet_composer/views.py
+++ diet_composer/views.py
@@ -2,17 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-user = [
-    {'username': "tm050288",
-     'email': "tm@gmail.com",
-     'password': "12345",
-     'reg_date': "12345"},
-]
-
 
 def home(request):
     return render(request, 'diet_composer/home.html')
 
 
 def about(request):
-    return render(request, 'diet_composer/about.html')
+    return render(request, 'diet_composer/about.html', {'title': "About"})
