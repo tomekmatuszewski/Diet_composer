e01d5e70a5a3af9a79e7eafdf3f73b73484cdd99
TomekM <tomasz_matuszewski@o2.pl>
2020-09-11 21:21:50 +0200

    new test url

--- apps/users/tests/test_urls.py
+++ apps/users/tests/test_urls.py
@@ -19,3 +19,15 @@ class TestUrls:
     def test_logout_url(self):
         path = reverse("logout")
         assert resolve(path).view_name == "logout"
+
+    def test_password_reset_url(self):
+        path = reverse("password_reset")
+        assert resolve(path).view_name == "password_reset"
+
+    def test_password_reset_done_url(self):
+        path = reverse("password_reset_done")
+        assert resolve(path).view_name == "password_reset_done"
+
+    def test_password_reset_confirm_url(self):
+        path = reverse("password_reset_complete")
+        assert resolve(path).view_name == "password_reset_complete"
